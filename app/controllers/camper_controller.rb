0acdbf1c84303300b3c07d882299ee2db691c4b9
leela khadka <khadkaleela3@gmail.com>
2022-10-19 13:23:47 -0700

    commit tree 70c289e55c9644f384a11499d1ae41b591059129

---
+++ app/controllers/camper_controller.rb
@@ -0,0 +1,5 @@
+class CampersController < ApplicationController
+end
+
+
+
---
+++ app/models/camper.rb
@@ -0,0 +1,12 @@
+class Camper < ApplicationRecord
+    has_many :signups
+has_many :activities, through: :signups
+
+validates :name, presence: true
+validates :age, numericality: {greater_than: 8, less_than: 18}
+#validates :age, numericality: {less_than: 18}
+
+end
+
+
+
